a55b8ea48ac40ce442b4b54ccd9267a018806188
padmavathi83 <33541039+padmavathi83@users.noreply.github.com>
2026-04-02 22:52:51 +0530

    feat: Create network.tf

---
+++ infra/network.tf
@@ -0,0 +1,11 @@
+resource "google_compute_network" "vpc_network" {
+  name                    = "my-vpc-network"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "subnet" {
+  name          = "my-subnet"
+  ip_cidr_range = "10.0.1.0/24"
+  region        = "us-central1"
+  network       = google_compute_network.vpc_network.id
+}
