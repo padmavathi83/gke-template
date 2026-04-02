83b5b7ba6c3d21eabb1eec7987f28845306ccff6
padmavathi83 <33541039+padmavathi83@users.noreply.github.com>
2026-04-02 22:58:14 +0530

    fix: Create variables.tf

---
+++ infra/variables.tf
@@ -0,0 +1,11 @@
+variable "project_id" {
+  description = "The ID of the Google Cloud project"
+  type        = string
+  default = "project-eb9a3368-5e31-47ac-837"
+}
+
+variable "region" {
+  description = "The region to deploy resources to"
+  type        = string
+  default     = "us-central1"
+}
