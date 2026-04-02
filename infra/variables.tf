b1625e7996e2d8eb216c177c2371a76c566c9ff0
padmavathi83 <33541039+padmavathi83@users.noreply.github.com>
2026-04-02 23:04:41 +0530

    fix: Update variables.tf

--- infra/variables.tf
+++ infra/variables.tf
@@ -9,3 +9,21 @@ variable "region" {
   type        = string
   default     = "us-central1"
 }
+variable "subnet_ip" {
+  description = "The IP range for the subnet"
+  type        = string
+}
+variable "secondary_ranges_pods_ip" {
+  description = "IP range for GKE Pods secondary range"
+  type        = string
+}
+
+variable "secondary_ranges_services_ip" {
+  description = "IP range for GKE Services secondary range"
+  type        = string
+}
+variable "environment" {
+  description = "The environment name (e.g. dev, prod)"
+  type        = string
+  default     = "dev"
+}
