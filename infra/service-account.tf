490da367fecf8078eb97a663e5e7259a29a394cb
padmavathi83 <33541039+padmavathi83@users.noreply.github.com>
2026-04-03 10:22:58 +0530

    Update IAM members to use service account directly
    Replaced workload identity pool IAM members with direct service account members.

--- infra/service-account.tf
+++ infra/service-account.tf
@@ -1,41 +1,11 @@
-
-resource "google_iam_workload_identity_pool" "github" {
-  project                   = var.project_id
-  workload_identity_pool_id = "github-pool-sa"
-  display_name              = "GitHub Actions Pool"
-  description               = "Identity pool for GH Actions to access GCP resources"
-}
-
-resource "google_iam_workload_identity_pool_provider" "github" {
-  workload_identity_pool_id          = google_iam_workload_identity_pool.github.workload_identity_pool_id
-  workload_identity_pool_provider_id = "github-actions-provider"
-  display_name                       = "GitHub Actions Provider"
-  description                        = "GitHub Actions identity pool provider"
-  #   disabled                           = true
-  attribute_condition = <<EOT
-    assertion.repository_owner_id == "33541039" &&
-    attribute.repository == "padmavathi83/gke-template"
-EOT
-  attribute_mapping = {
-    "google.subject"       = "assertion.sub"
-    "attribute.actor"      = "assertion.actor"
-    "attribute.aud"        = "assertion.aud"
-    "attribute.repository" = "assertion.repository"
-  }
-  oidc {
-    issuer_uri = "https://token.actions.githubusercontent.com"
-  }
-}
-
-resource "google_project_iam_member" "github" {
+resource "google_project_iam_member" "github_artifact" {
   project = var.project_id
   role    = "roles/artifactregistry.createOnPushWriter"
-  member = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.github.name}/*"
+  member  = "serviceAccount:github-sa@project-eb9a3368-5e31-47ac-837.iam.gserviceaccount.com"
 }
 
 resource "google_project_iam_member" "github_gke" {
   project = var.project_id
   role    = "roles/container.admin"
-  member  = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.github.name}/*"
+  member  = "serviceAccount:github-sa@project-eb9a3368-5e31-47ac-837.iam.gserviceaccount.com"
 }
-
