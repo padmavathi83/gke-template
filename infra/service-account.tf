ce669e0eab91dee6bff3fd4e673d674c1d01bd57
padmavathi83 <33541039+padmavathi83@users.noreply.github.com>
2026-04-03 10:14:24 +0530

    Update GitHub Actions IAM resources for service account

--- infra/service-account.tf
+++ infra/service-account.tf
@@ -1,14 +1,14 @@
 
 resource "google_iam_workload_identity_pool" "github" {
   project                   = var.project_id
-  workload_identity_pool_id = "github-pool"
+  workload_identity_pool_id = "github-pool-sa"
   display_name              = "GitHub Actions Pool"
   description               = "Identity pool for GH Actions to access GCP resources"
 }
 
 resource "google_iam_workload_identity_pool_provider" "github" {
   workload_identity_pool_id          = google_iam_workload_identity_pool.github.workload_identity_pool_id
-  workload_identity_pool_provider_id = "github-provider"
+  workload_identity_pool_provider_id = "github-actions-provider"
   display_name                       = "GitHub Actions Provider"
   description                        = "GitHub Actions identity pool provider"
   #   disabled                           = true
